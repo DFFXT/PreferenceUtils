4796421566f073ce951dd3334a2139584619fae7
feiqin <feiqin@iflytek.com>
2021-08-02 20:51:44 +0800

    fix: application

--- app/src/main/java/com/fxffxt/preferenceutils/MainActivity.kt
+++ app/src/main/java/com/fxffxt/preferenceutils/MainActivity.kt
@@ -1,26 +1,19 @@
 package com.fxffxt.preferenceutils
 
-import android.content.ComponentName
-import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import android.widget.PopupWindow
 import androidx.appcompat.app.AppCompatActivity
 import com.fxffxt.preferen.Config
-import com.fxffxt.preferen.noneNull
 import com.fxffxt.preferen.nullable
-import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Config.ctx = this
+        Config.ctx = this.application
         findViewById<View>(R.id.tv_text).setOnClickListener {
             log()
         }
-
     }
     private fun log() {
         val config = Student()
